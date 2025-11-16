8412126db781ae5febae31fbc14126d5934e267b
JoseRomualdo <jromualdo3@hotmail.com>
2025-11-16 09:33:41 -0300

    refactor(UrlRepository): add findByurlOriginal.

--- src/main/java/com/encurtadorurl/encurtadorurl/repository/UrlRepository.java
+++ src/main/java/com/encurtadorurl/encurtadorurl/repository/UrlRepository.java
@@ -4,6 +4,10 @@ import com.encurtadorurl.encurtadorurl.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
+
+    Optional<Url> findByurlOriginal(String urlFull);
 }
