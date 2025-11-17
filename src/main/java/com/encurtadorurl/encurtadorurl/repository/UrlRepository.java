737d2e34bef1d9b7738d004546b5bee5442f95a3
JoseRomualdo <jromualdo3@hotmail.com>
2025-11-17 11:08:26 -0300

    refactor(urlRepository): add query for sequence and findByurlShort.

--- src/main/java/com/encurtadorurl/encurtadorurl/repository/UrlRepository.java
+++ src/main/java/com/encurtadorurl/encurtadorurl/repository/UrlRepository.java
@@ -2,6 +2,7 @@ package com.encurtadorurl.encurtadorurl.repository;
 
 import com.encurtadorurl.encurtadorurl.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,16 @@ import java.util.Optional;
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
 
+    /**
+     * Execute :
+     * CREATE SEQUENCE url_short_seq
+     *     START WITH 1
+     *     INCREMENT BY 1;
+     * This is necessary for postgres
+     */
+    @Query(value = "SELECT nextval('url_short_seq')", nativeQuery = true)
+    Long getNextUrlShortenerId();
+
     Optional<Url> findByurlOriginal(String urlFull);
+    Optional<Url> findByurlShort(String urlShort);
 }
